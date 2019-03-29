95d49c3c132097bcad11dfc97475c09a819e4a6b
lander moreno <lander.morenoalcaide@altran.com>
2019-03-29 14:37:47 +0100

    deleted error handle method

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
@@ -26,16 +25,6 @@ def create_tables():
     db.create_all()
 
 
-@jwt.expired_token_loader
-def my_expired_token_callback(self, expired_token):
-    token_type = expired_token['type']
-    return jsonify({
-        'status': 401,
-        'sub_status': 42,
-        'msg': 'The {} token has expired'.format(token_type)
-    }), 401
-
-
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
@@ -56,16 +45,6 @@ app.add_url_rule('/token/refresh', view_func=TokenRefresh.as_view('token_refresh
 app.add_url_rule('/cities', view_func=Cities.as_view('cities'))
 app.add_url_rule('/userCity', view_func=UserCities.as_view('user_city'))
 
-# api.add_resource(endpoints.UserRegistration, '/register')
-# api.add_resource(endpoints.UserLogin, '/login')
-# api.add_resource(endpoints.UserLogoutAccess, '/logout/access')
-# api.add_resource(endpoints.UserLogoutRefresh, '/logout/refresh')
-# api.add_resource(endpoints.TokenRefresh, '/token/refresh')
-# api.add_resource(endpoints.Cities, '/cities')
-# api.add_resource(endpoints.UserCities, '/userCity')
-
 
 def create_app():
     return app
-# if __name__ == '__main__':
-#     app.run(debug=True)
